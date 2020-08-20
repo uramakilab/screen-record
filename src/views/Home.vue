361d645f08ccaa5686034b58062a1a525fe90bc2
leoruas <leo.coelho.ruas@gmail.com>
2020-08-20 12:53:52 -0300

    started view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-row justify="space-around" class="mt-3">
+      <v-btn>
+        <v-icon left>mdi-account</v-icon>
+        Start Camera</v-btn>
+
+      <v-btn>
+        <v-icon left color="red">mdi-circle</v-icon>
+        Record</v-btn>
+      <v-btn>
+        <v-icon left>mdi-monitor</v-icon>
+        Start Screen Share</v-btn>
+    </v-row>
+    <v-row justify="space-around" style="margin-top: 30px">
+      <video width="45%" class="video" autoplay></video>
+
+      <video width="45%" class="video" autoplay></video>
+    </v-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
+
+<style scoped>
+.video {
+  border: 1px solid black;
+}
+</style>
