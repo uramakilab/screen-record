a911ce6f39330c7305ee9e757cd8f4610e0f3ef3
leoruas <leo.coelho.ruas@gmail.com>
2020-08-21 10:34:33 -0300

    changed buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <v-row justify="space-around" class="mt-3">
+  <div style="background-color: #b9cceb; height: 100%">
+    <!-- <v-row justify="space-around" class="mt-3">
       <v-btn @click="!webcam?startWebCamCapture():stopWebCamCapture()">
         <v-icon left>mdi-account</v-icon>Start Camera
       </v-btn>
@@ -16,18 +16,88 @@
       <v-btn @click="!audio?startAudioCapture():stopAudioCapture()">
         <v-icon left>{{audio?'mdi-stop':'mdi-play'}}</v-icon>Start Audio Capture
       </v-btn>
-    </v-row>
+    </v-row>-->
     <v-row justify="space-around" style="margin-top: 30px">
       <video width="45%" id="webcam" class="video" autoplay></video>
 
       <video width="45%" id="screen" class="video" autoplay></video>
 
       <audio id="audio" autoplay></audio>
+      
       <!-- Saved Media  -->
       <video v-if="mediaWebCam" :src="mediaWebCam" width="45%" id="recordWC" class="video" controls></video>
       <video v-if="mediaScreen" :src="mediaScreen" width="45%" id="record" class="video" controls></video>
       <audio v-if="mediaAudio" :src="mediaAudio" id="Recordaudio" controls></audio>
     </v-row>
+    
+    <!-- Speed Dial -->
+    <v-speed-dial v-model="fab" fixed class="mr-3" bottom right open-on-hover>
+      <template v-slot:activator>
+        <v-tooltip left>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-model="fab"
+              large
+              fab
+              class="btn-fix"
+              v-bind="attrs"
+              v-on="on"
+              @click="!recording?startRecord():stopRecord()"
+            >
+              <v-icon color="red">{{recording ? 'mdi-close-circle' : 'mdi-circle'}}</v-icon>
+            </v-btn>
+          </template>
+          <span>{{recording ? 'Stop Recording' : 'Record'}}</span>
+        </v-tooltip>
+      </template>
+
+      <v-tooltip left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            @click="!webcam?startWebCamCapture():stopWebCamCapture()"
+            fab
+            small
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon v-if="!webcam">mdi-account</v-icon>
+            <v-icon v-else>mdi-account-cancel</v-icon>
+          </v-btn>
+        </template>
+        <span>{{webcam? 'Stop' : 'Start'}} Camera</span>
+      </v-tooltip>
+
+      <v-tooltip left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            @click="!screen?startScreenCapture():stopScreenCapture()"
+            fab
+            small
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon v-if="!screen">mdi-monitor</v-icon>
+            <v-icon v-else>mdi-monitor-off</v-icon>
+          </v-btn>
+        </template>
+        <span>{{audio ? 'Stop' : 'Start'}} Screen Share</span>
+      </v-tooltip>
+
+      <v-tooltip left>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            @click="!audio?startAudioCapture():stopAudioCapture()"
+            fab
+            small
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>{{audio?'mdi-volume-mute':'mdi-volume-high'}}</v-icon>
+          </v-btn>
+        </template>
+        <span>{{audio ? 'Stop' : 'Start'}} Audio Capture</span>
+      </v-tooltip>
+    </v-speed-dial>
   </div>
 </template>
 
@@ -43,21 +113,22 @@ export default {
     audio: false,
     options: {
       video: true,
-      audio: true
+      audio: true,
     },
     recordWebCam: null,
     recordScreen: null,
     recordAudio: null,
     mediaWebCam: null,
     mediaScreen: null,
-    mediaAudio: null
+    mediaAudio: null,
+    fab: false,
   }),
   methods: {
     startWebCamCapture() {
       this.webcam = true;
       navigator.mediaDevices
         .getUserMedia(this.options)
-        .then(mediaStreamObj => {
+        .then((mediaStreamObj) => {
           let video = document.getElementById("webcam");
           if ("srcObject" in video) {
             video.srcObject = mediaStreamObj;
@@ -67,7 +138,7 @@ export default {
           this.recordWebCam = new MediaRecorder(mediaStreamObj);
           let recordingWebCam = [];
 
-          this.recordWebCam.ondataavailable = function(ev) {
+          this.recordWebCam.ondataavailable = function (ev) {
             recordingWebCam.push(ev.data);
           };
 
@@ -77,7 +148,7 @@ export default {
             this.mediaWebCam = window.URL.createObjectURL(blob);
           };
         })
-        .catch(err => {
+        .catch((err) => {
           console.error("Error:" + err);
         });
     },
@@ -85,14 +156,14 @@ export default {
       this.webcam = false;
       let videoElem = document.getElementById("webcam");
       let tracks = videoElem.srcObject.getTracks();
-      tracks.forEach(track => track.stop());
+      tracks.forEach((track) => track.stop());
       videoElem.srcObject = null;
     },
     startScreenCapture() {
       this.screen = true;
       navigator.mediaDevices
         .getDisplayMedia(this.options)
-        .then(mediaStreamObj => {
+        .then((mediaStreamObj) => {
           let video = document.getElementById("screen");
           if ("srcObject" in video) {
             video.srcObject = mediaStreamObj;
@@ -102,7 +173,7 @@ export default {
           this.recordScreen = new MediaRecorder(mediaStreamObj);
           let recordingScreen = [];
 
-          this.recordScreen.ondataavailable = function(ev) {
+          this.recordScreen.ondataavailable = function (ev) {
             recordingScreen.push(ev.data);
           };
 
@@ -112,7 +183,7 @@ export default {
             this.mediaScreen = window.URL.createObjectURL(blob);
           };
         })
-        .catch(err => {
+        .catch((err) => {
           console.error("Error:" + err);
           return null;
         });
@@ -121,18 +192,18 @@ export default {
       this.screen = false;
       let videoElem = document.getElementById("screen");
       let tracks = videoElem.srcObject.getTracks();
-      tracks.forEach(track => track.stop());
+      tracks.forEach((track) => track.stop());
       videoElem.srcObject = null;
     },
     startAudioCapture() {
       this.audio = true;
       let audioOptions = {
         video: false,
-        audio: true
+        audio: true,
       };
       navigator.mediaDevices
         .getUserMedia(audioOptions)
-        .then(mediaStreamObj => {
+        .then((mediaStreamObj) => {
           let video = document.getElementById("audio");
           if ("srcObject" in video) {
             video.srcObject = mediaStreamObj;
@@ -142,7 +213,7 @@ export default {
           this.recordAudio = new MediaRecorder(mediaStreamObj);
           let recordingAudio = [];
 
-          this.recordAudio.ondataavailable = function(ev) {
+          this.recordAudio.ondataavailable = function (ev) {
             recordingAudio.push(ev.data);
           };
 
@@ -152,7 +223,7 @@ export default {
             this.mediaAudio = window.URL.createObjectURL(blob);
           };
         })
-        .catch(err => {
+        .catch((err) => {
           console.error("Error:" + err);
         });
     },
@@ -160,7 +231,7 @@ export default {
       this.audio = false;
       let AudioEle = document.getElementById("audio");
       let tracks = AudioEle.srcObject.getTracks();
-      tracks.forEach(track => track.stop());
+      tracks.forEach((track) => track.stop());
       AudioEle.srcObject = null;
     },
     startRecord() {
@@ -188,8 +259,8 @@ export default {
       if (this.recordAudio) {
         this.recordAudio.stop();
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
