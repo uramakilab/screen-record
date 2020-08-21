b19b1bd05d632eba1439473670ac467640c5a3e8
leoruas <leo.coelho.ruas@gmail.com>
2020-08-21 12:02:21 -0300

    wip firebase

--- src/main.js
+++ src/main.js
@@ -2,7 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase/app'
 import vuetify from './plugins/vuetify';
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  storageBucket: process.env.STORAGE_BUCKET,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.APP_ID
+}
+
+firebase.initializeApp(firebaseConfig)
+// var storage = firebase.storage()
 
 Vue.config.productionTip = false
 
